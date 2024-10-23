8f6160ba8b3eb9478402cac407f18d3aae22efc8
Tapan4803 <Tapanpanjabi8@gmail.com>
2024-10-24 01:47:49 +0530

    added database

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
